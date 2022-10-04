d404749a218ede3179d3acd7dafeba140bce2bfb
Alex Damovski <adamovsk@my.centennialcollege.ca>
2022-10-03 21:06:24 -0400

    Added Mongo Atlas Configuration

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+// export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI  = "mongodb+srv://ADamovski:w4H2riwnfdIKEIPn@comp229.kofpvto.mongodb.net/media1?retryWrites=true&w=majority"
